f2dea78912e94581cc11b00c3692f99452ad614c
YARELY BELEN JIMENEZ DIEGO <154484018+YarelyJ@users.noreply.github.com>
2025-11-05 09:44:10 -0600

    Update vite-env.d.tsx

--- frontend/src/vite-env.d.tsx
+++ frontend/src/vite-env.d.tsx
@@ -1,15 +1,9 @@
 /// <reference types="vite/client" />
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
 }
 
-interface ImportMetaEnv {
-  // Agrega aquí las variables de entorno que usas
-  // (Deben empezar con VITE_ por seguridad)
-  // Por ejemplo, si usas VITE_API_URL en 'api.ts':
-  readonly VITE_API_URL: string;
-
-  // Agrega cualquier otra que necesites
-  // readonly VITE_OTRA_VARIABLE: string;
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
